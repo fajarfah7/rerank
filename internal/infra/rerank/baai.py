6f0bb0c711666ebb04251740fb1b4388d310b2e3
Fajar Fahrurozi <fajar.fah7@gmail.com>
2026-01-23 11:18:54 +0700

    implement to take top_n data

--- internal/infra/rerank/baai.py
+++ internal/infra/rerank/baai.py
@@ -1,11 +1,15 @@
 from sentence_transformers import CrossEncoder
 from internal.domain.document import Document
+import os
 
 class RerankBAAI:
     def __init__(self, model: CrossEncoder):
         self.model = model
 
     def rerank(self, query: str, documents: list) -> list[Document]:
+        top_n = int(os.getenv("TOP_N"))
+        if top_n <= 0:
+            top_n = 3
 
         pairs = [(query, doc) for doc in documents]
 
@@ -17,8 +21,10 @@ class RerankBAAI:
             reverse=True,
         )
 
+        top_k = ranked[:top_n]
+
         result = []
-        for doc, score in ranked:
+        for doc, score in top_k:
             print(f"{score:.4f} | {doc}")
             document = Document(document=doc, score=round(score, 4))
             result.append(document)
